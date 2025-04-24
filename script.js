5c0bbfc026cdf970420c4d9b5300ec3db5e59384
akvimys1 <nanivskiyr@gmail.com>
2025-04-24 16:18:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,12 +55,10 @@ setTimeout(() => {
 
   panel.classList.add("show");
 
-  panel.style.backgroundImage = "linear-gradient(to bottom, black, #01017f, black)";
+  panel.style.backgroundImage = `url(${person.background})`;
   panel.style.backgroundSize = "cover";
   panel.style.backgroundPosition = "center";
   panel.style.backgroundRepeat = "no-repeat";
-  panel.style.backgroundBlendMode = "normal"; // нормальный режим без затемнения
-  panel.style.backgroundColor = ""; // убираем затемнение
 
   panel.classList.add("show");
   
